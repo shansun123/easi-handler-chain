fcfb8e20b85a14ac9ff88096ceaafc1499113f3a
Chris <lanbo.xj@taobao.com>
2013-01-08 11:02:11 +0800

    Optional define.

---
+++ src/com/taobao/inventory/common/func/Option.java
@@ -0,0 +1,12 @@
+/**
+ * 
+ */
+package com.taobao.inventory.common.func;
+
+/**
+ * @author Chris
+ *
+ */
+public class Option<T> {
+	T reference;	
+}
---
+++ src/com/taobao/inventory/common/func/package-info.java
@@ -0,0 +1,5 @@
+/**
+ * @author Chris
+ * 一些FP的工具类
+ */
+package com.taobao.inventory.common.func;
