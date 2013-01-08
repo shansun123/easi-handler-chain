5b1928171967d9fc12a94af24fe272b6969ad85a
Chris <lanbo.xj@taobao.com>
2013-01-08 14:39:22 +0800

    Wrote some comments.

--- src/com/taobao/inventory/common/process/appender/Appendable.java
+++ src/com/taobao/inventory/common/process/appender/Appendable.java
@@ -3,18 +3,47 @@
  */
 package com.taobao.inventory.common.process.appender;
 
+import com.taobao.inventory.common.process.HandlerChain;
+import com.taobao.inventory.common.process.handler.CompositeHandler;
 import com.taobao.inventory.common.process.handler.Handler;
+import com.taobao.inventory.common.process.handler.MutexHandler;
 import com.taobao.inventory.common.process.interceptor.HandlerInterceptor;
 
 /**
- * @author Chris
+ * <pre>
+ * 标识某个类是否支持添加处理节点
+ * </pre>
  * 
+ * 目前支持该特性的类有：
+ * <li>{@link HandlerChain}</li> 
+ * <li>{@link CompositeHandler}</li>
+ * <li>{@link MutexHandler}</li>
+ * 
+ * @author Chris
  */
 public interface Appendable<T> {
-	
+
+	/**
+	 * 添加一个处理节点
+	 * 
+	 * @param handler 处理节点
+	 * @return
+	 */
 	Appendable<T> appendHandler(Handler<T> handler);
-	
+
+	/**
+	 * 添加一个处理节点，并指定该节点的下一个节点
+	 * 
+	 * @param handler 处理节点
+	 * @param successor 该节点的下一个节点
+	 * @return
+	 */
 	Appendable<T> appendHandler(Handler<T> handler, Handler<T> successor);
-	
+
+	/**
+	 * 添加一个处理拦截器，该拦截器会应用到{@link HandlerChain}中每一个处理节点
+	 * @param interceptor 拦截器
+	 * @return
+	 */
 	Appendable<T> appendInterceptor(HandlerInterceptor<T> interceptor);
 }
--- src/com/taobao/inventory/common/process/config/HandlerConfig.java
+++ src/com/taobao/inventory/common/process/config/HandlerConfig.java
@@ -6,19 +6,36 @@ package com.taobao.inventory.common.process.config;
 import com.taobao.inventory.common.process.context.AbstractContext;
 
 /**
- * @author Chris
+ * <pre>
+ * 处理节点的配置信息，如可以设置是否忽略异常等
+ * </pre>
  * 
+ * 除已定义的配置项外，还可以自定义配置，通过{@link HandlerConfig#put(String, Object)}设置。
+ * 
+ * @author Chris
  */
 public class HandlerConfig<T> extends AbstractContext implements Configuable {
+	/**
+	 * 是否忽略该节点异常，默认不忽略
+	 */
 	private boolean ignoreException = false;
 	
+	/**
+	 * 是否拒绝拦截器监视，默认不拒绝
+	 */
 	private boolean refuseInterceptor = false;
 
+	/**
+	 * 全局默认节点配置
+	 */
 	static private HandlerConfig<Object> DEFAULT_CONFIG = new HandlerConfig<Object>();
 
 	private HandlerConfig() {
 	}
 
+	/**
+	 * 
+	 */
 	public HandlerConfig<T> config() {
 		return new HandlerConfig<T>();
 	}
